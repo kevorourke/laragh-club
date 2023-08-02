848b1a581d82d0703f3cc7ca643bca5ff3f6849e
Kevin <kev.orourke4@gmail.com>
2023-08-02 15:45:07 +0100

    Navbar update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/navbar";
 import SupabaseProvider from "@/supabase/SupabaseProvider";
 import { getSession } from "@/supabase/supabase-server";
 
