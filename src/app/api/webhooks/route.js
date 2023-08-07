9ddd07fc1d2a3f62af5d5f1b2c3cb5edee5b56ad
Kevin <kev.orourke4@gmail.com>
2023-08-07 01:45:36 +0100

    Changes

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,19 +1,31 @@
 import Stripe from "stripe";
 import { createNewPaymentRecord } from "@/supabase/supabase-admin";
 
-export async function POST(req) {
-  const body = await req.text();
-  const sig = req.headers["stripe-signature"];
+export default async function handleStripeWebhook(req, res) {
+  // Log the entire request object to diagnose its structure
+  console.log("Request object:", req);
+
+  // Safely attempt to extract the Stripe-Signature header
+  const sig = req && req.headers ? req.headers["stripe-signature"] : null;
+
+  // If for some reason we still don't have a signature, log it and exit early
+  if (!sig) {
+    console.error("No Stripe signature found in the request.");
+    res.status(400).json({ error: "Missing Stripe signature" });
+    return;
+  }
+
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   let event;
 
   try {
-    if (!sig || !webhookSecret) return;
-    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    if (!webhookSecret) throw new Error("No webhook secret provided");
+    event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
   } catch (err) {
-    console.log(`Error message: ${err.message}`);
-    return new Response(`Webhook Error: ${err.message}`, { status: 400 });
+    console.error(`Error message: ${err.message}`);
+    res.status(400).json({ error: `Webhook Error: ${err.message}` });
+    return;
   }
 
   if (event.type === "checkout.session.completed") {
@@ -22,17 +34,19 @@ export async function POST(req) {
       let data = {};
       data.amount = checkoutSession.amount_total;
       data.member_ids = JSON.parse(checkoutSession.metadata.memberIds);
-      await createNewPaymentRecord(data); // ensure this is awaited if it's async
-      console.log(checkoutSession);
+      await createNewPaymentRecord(data);
+      console.log("Checkout Session:", checkoutSession);
     } catch (error) {
-      console.log(error);
-      return new Response(
-        "Webhook handler failed. View your nextjs function logs.",
-        {
-          status: 400,
-        }
+      console.error(
+        "Error processing checkout.session.completed event:",
+        error
       );
+      res.status(400).json({
+        error: "Webhook handler failed. Check server logs for more details.",
+      });
+      return;
     }
   }
-  return new Response(JSON.stringify({ received: true }));
+
+  res.json({ received: true });
 }
