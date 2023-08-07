2e968fdd79a9f6b3a9a6f9e6a40f874f0948a446
Kevin <kev.orourke4@gmail.com>
2023-08-07 02:18:09 +0100

    Testing

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,33 +1,19 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+import Stripe from "stripe";
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    res.status(405).end(); // Method not allowed
-    return;
-  }
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15",
+});
 
-  const buf = await buffer(req);
-  const payload = buf.toString("utf-8");
+export async function POST(req) {
   let event;
+  const rawBody = await req.text(); // Have also tried using await buffer(req) from 'micro' but no luck
+  const stripeHeader = req.headers.get("stripe-signature"); // This returns undefined always....
+  event = stripe.webhooks.constructEvent(
+    rawBody,
+    stripeHeader,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY
+  );
 
-  if (endpointSecret) {
-    const signature = req.headers["stripe-signature"];
-    try {
-      event = stripe.webhooks.constructEvent(
-        payload,
-        signature,
-        endpointSecret
-      );
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      return res.status(400).end(); // Bad request response
-    }
-  } else {
-    event = JSON.parse(payload);
-  }
-
-  // Handle the event
   switch (event.type) {
     case "checkout.session.completed":
       try {
@@ -52,16 +38,10 @@ export default async function handler(req, res) {
       // Unexpected event type
       console.log(`Unhandled event type ${event.type}.`);
   }
-
-  res.json({ received: true }); // Return a response to acknowledge receipt of the event
 }
 
-// This helper function is necessary because the Stripe webhook's signature verification requires
-// the raw body, not the parsed body that Next.js provides by default.
-async function buffer(readable) {
-  const chunks = [];
-  for await (const chunk of readable) {
-    chunks.push(chunk);
-  }
-  return Buffer.concat(chunks);
-}
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
